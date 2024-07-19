42441158b66b578d7a0fb0d76098c4492e23e9f3
Trieu Ngoc Tai <ngoctai28112k@gmail.com>
2024-07-19 14:45:00 +0700

    chặn các request chưa được khai báo trong route

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,16 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Illuminate\Foundation\Auth\User as Authenticatable;
 use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,10 @@ use Illuminate\Auth\Middleware\Authenticate;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::apiResource('user', UserController::class);
-//Route::get('/login', function (Request $request) {
-//    return '123';
-//});
+Route::group(['prefix' => 'auth'], function($router) {
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [UserController::class, 'login']);
+});
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('logout', [UserController::class, 'logout']);
+});
